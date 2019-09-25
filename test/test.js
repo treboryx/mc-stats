b4a5495ba3b0d79fbef9a939c2406cdb29bcc8d4
Robert <treboryx@gmail.com>
2019-09-26 00:27:55 +0200

    seperated tests from the main file

---
+++ test/test.js
@@ -0,0 +1,17 @@
+const mc = require("../app.js");
+
+const main = async () => {
+  // util.hypixelPlayer("Dinnerbone").then(r => console.log(r));
+  // util.blocksmc("_IPlozS").then(R => console.log(r));
+  // util.funcraft("nathan818").then(r => console.log(r));
+  // util.mineplex("xeiu").then(r => console.log(r));
+  // util.manacube("KaiYara12").then(r => console.log(r));
+  // util.minesaga("doitnowornever").then(r => console.log(r));
+  // util.gommehd("123123").then(r => console.log(r));
+  // util.timolia("XxSt3gsfanxX").then(r => console.log(r));
+  // util.minemen("Cleid").then(r => console.log(r));
+  // util.veltpvp("DissTracks").then(r => console.log(r));
+  mc.universocraft("ImOvee_Idk").then(r => console.log(r));
+};
+  
+main();
