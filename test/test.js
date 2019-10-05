6a8791f2d9956582035588c82521c85a9ac7defd
Robert <treboryx@gmail.com>
2019-10-05 22:39:36 +0200

    Added boosters info for hypixel, some player checks

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ const mc = require("../app.js");
 
 const main = async () => {
   // util.hypixelPlayer("Dinnerbone").then(r => console.log(r));
-  // util.blocksmc("_IPlozS").then(R => console.log(r));
+  mc.blocksmc("_IPlozSawdadawd").then(r => console.log(r));
   // util.funcraft("nathan818").then(r => console.log(r));
   // util.mineplex("xeiu").then(r => console.log(r));
   // util.manacube("KaiYara12").then(r => console.log(r));
@@ -11,7 +11,8 @@ const main = async () => {
   // util.timolia("XxSt3gsfanxX").then(r => console.log(r));
   // util.minemen("Cleid").then(r => console.log(r));
   // util.veltpvp("DissTracks").then(r => console.log(r));
-  mc.universocraft("ImOvee_Idk").then(r => console.log(r));
+  // mc.universocraft("ImOvee_Idk").then(r => console.log(r));
+  // mc.hypixelBoosters().then(r => console.log(r));
 };
   
 main();
