e352846391135e000aa3a653feeeb8eeb898ba3d
Tj <talle117@eq.edu.au>
2019-12-02 23:11:36 +1000

    HiveMC Endpoints
    Completed all HiveMC game endpoints.

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ const mc = require("../app.js");
 
 const main = async () => {
   // mc.hypixelPlayer("Dinnerbone").then(r => console.log(r));
-  mc.blocksmc("_IPlozSawdadawd").then(r => console.log(r));
+  // mc.blocksmc("_IPlozSawdadawd").then(r => console.log(r));
   // mc.funcraft("nathan818").then(r => console.log(r));
   // mc.mineplex("xeiu").then(r => console.log(r));
   // mc.manacube("KaiYara12").then(r => console.log(r));
@@ -14,13 +14,42 @@ const main = async () => {
   // mc.universocraft("ImOvee_Idk").then(r => console.log(r));
   // mc.hypixelBoosters().then(r => console.log(r));
 
-  mc.hivemc("Gooogle_it", "profile").then(r => console.log(r));
-  mc.hivemc("jonas2246", "SG").then(r => console.log(r));
-  mc.hivemc("Tutle", "BP").then(r => console.log(r));
-  mc.hivemc("Ingola", "CAI").then(r => console.log(r));
-  mc.hivemc("Endlen", "CR").then(r => console.log(r));
-  mc.hivemc("kaiurii", "DR").then(r => console.log(r));
-  mc.hivemc("Limimin", "HB").then(r => console.log(r));
+  // mc.hivemc("Gooogle_it", "profile").then(r => console.log(r));
+  // mc.hivemc("jonas2246", "SG").then(r => console.log(r));
+  // mc.hivemc("Tutle", "BP").then(r => console.log(r));
+  // mc.hivemc("Ingola", "CAI").then(r => console.log(r));
+  // mc.hivemc("Endlen", "CR").then(r => console.log(r));
+  // mc.hivemc("kaiurii", "DR").then(r => console.log(r));
+  // mc.hivemc("Limimin", "HB").then(r => console.log(r));
+  // mc.hivemc("Limimin", "HB").then(r => console.log(r));
+  // mc.hivemc("Caro", "HERO").then(r => console.log(r));
+  // mc.hivemc("Spenken", "HIDE").then(r => console.log(r));
+  // mc.hivemc("AslanThePro", "OITC").then(r => console.log(r));
+  // mc.hivemc("Demon2921", "SP").then(r => console.log(r));
+  // mc.hivemc("HeinzBeans", "TIMV").then(r => console.log(r));
+  // mc.hivemc("Rinjani", "SKY").then(r => console.log(r));
+  // mc.hivemc("Mcae", "DRAW").then(r => console.log(r));
+  // mc.hivemc("Varsel", "SLAP").then(r => console.log(r));
+  // mc.hivemc("Mackan__", "EF").then(r => console.log(r));
+  // mc.hivemc("Conga_", "MM").then(r => console.log(r));
+  // mc.hivemc("ShooterGHP", "GRAV").then(r => console.log(r));
+  // mc.hivemc("Bgbros", "RR").then(r => console.log(r));
+  // mc.hivemc("RageStateOfMind", "GNT").then(r => console.log(r));
+  // mc.hivemc("iiAnass", "SGN").then(r => console.log(r));
+  // mc.hivemc("Fcssy", "BD").then(r => console.log(r));
+  // mc.hivemc("IDontExquseYou", "SPL").then(r => console.log(r));
+  // mc.hivemc("Marleeey", "MIMV").then(r => console.log(r));
+  // mc.hivemc("HappyStateOfMind", "BED").then(r => console.log(r));
+
+
+
+
+  
+  
+
+
+
+
 };
   
 main();
